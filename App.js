a1635f6b4d9709eb202209d301a5696c55c7b070
ysancak <yusufsancakk@gmail.com>
2023-02-02 23:01:00 +0300

    changed messaging component with conversation

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Tabbar from './app/components/Tabbar'
 
 import Splash from './app/screens/Splash'
 import Home from './app/screens/Home'
-import Messaging from './app/screens/Messaging'
+import Conversations from './app/screens/Conversations'
 import Add from './app/screens/Add'
 import Likes from './app/screens/Likes'
 import Profile from './app/screens/Profile'
@@ -15,9 +15,9 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator tabBar={props => <Tabbar {...props} />} screenOptions={{ headerShown: false }} initialRouteName="Home">
+    <Tab.Navigator tabBar={props => <Tabbar {...props} />}  screenOptions={{ headerShown: false }} initialRouteName="Home">
         <Tab.Screen name="Home" component={Home} options={{  }} />
-        <Tab.Screen name="Messaging" component={Messaging} options={{  }} />
+        <Tab.Screen name="Conversations" component={Conversations} options={{  }} />
         <Tab.Screen name="Add" component={Add} options={{  }} />
         <Tab.Screen name="Likes" component={Likes} options={{  }} />
         <Tab.Screen name="Profile" component={Profile} options={{  }} />
